711820bb856675fee646179c021c4ac1742daff5
nestix6 <repkovsky.patrik@gmail.com>
2025-06-04 10:51:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 int main() {
   printf("Hello world");
+    int x = 5;
+    int y = 7;
+    int vysledok = sucet(x, y);
 
+    printf("Súčet %d a %d je %d\n", x, y, vysledok);
+
+    return 0;
 return 0
   }
+  
+int sucet(int a, int b) {
+    return a + b;
+}
+
